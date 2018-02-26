49a8e00c6ecf1339014bbb364e4ffd8904d35871
OlgaMardvilko <omardvilko@mail.ru>
2018-02-26 17:35:17 +0300

    task eight

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 module.exports = function getZerosCount(number, base) {
   let result;
 
-    for (let i = 2; i <= base; i++) {
-      if (base%i == 0) { 
+  for (let i = 2; i <= base; i++) {
+    if (base%i == 0) { 
         
-		let delitel = 0;
-        let zeros = 0;
-        let num = number;
+		  let delitel = 0;
+      let zeros = 0;
+      let num = number;
 
-        while (base%i === 0) {
-          base = Math.floor(base/i);
-          delitel++;
-        }
-
-        while (Math.floor(num/i) > 0) {
-          zeros += Math.floor(num/i);
-          num = Math.floor(num/i);
-        } 
-        result = Math.floor(zeros/delitel); 
+      while (base%i == 0) {
+        base = Math.floor(base/i);
+        delitel++;
       }
-    }        
 
-    return result;
+      while (Math.floor(num/i) > 0) {
+        zeros += Math.floor(num/i);
+        num = Math.floor(num/i);
+      }    
+      result = Math.floor(zeros/delitel); 
+    }
+  }       
+  return result;
 }
